78c59e095621eb68b02edbfba5637cff32b2ed73
XiaoLing <samuel.ling.xiao@qq.com>
2025-07-28 22:25:11 +0800

    add base exception

---
+++ opentrons_bot/errors.py
@@ -0,0 +1,9 @@
+class BaseException(Exception):
+    def __init__(self, msg: str) -> None:
+        self._msg = msg
+
+    def __str__(self) -> str:
+        return self._msg
+
+    def __repr__(self) -> str:
+        return self._msg
